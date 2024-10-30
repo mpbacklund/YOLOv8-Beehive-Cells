194fef8586a4bc25b3f8e659063363a80b4220bd
Matt Backlund <mpbacklund99@gmail.com>
2024-10-30 14:05:02 -0600

    got yolo up and going on my machine. Added if name == main to prevent module from restarting

--- v8_custom.py
+++ v8_custom.py
@@ -1,15 +1,19 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
-train_results = model.train(data='data.yaml',
-    epochs=100,
-    imgsz=640,
-    device=0)
+def main():
+    model = YOLO("yolov8n.pt")
+    train_results = model.train(data='data.yaml',
+        epochs=100,
+        imgsz=640,
+        device=0)
 
-# evaluate performace on the validation set
-metrics = model.val()
-# perform obj detection on image
-results = model("../data/obj/2013_07_28_CHBRC_010.png")
-results[0].show()
-# export format
-export_model = model.export(format="onnx")
+    # evaluate performace on the validation set
+    metrics = model.val()
+    # perform obj detection on image
+    results = model("datasets/test/2013_07_28_CHBRC_010.png")
+    results[0].show()
+    # export format
+    export_model = model.export(format="onnx")
+
+if __name__ == "__main__":
+    main()
